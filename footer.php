ae7a02383a93b514a72c71fb6c48645dbe4a81dd
Mohamadfars <147287843+Mohamadfars@users.noreply.github.com>
2025-04-20 21:09:50 +0300

    footer.php

---
+++ footer.php
@@ -0,0 +1,8 @@
+<footer class="footer">
+        <div class="container">
+            <p>© <?php echo date('Y'); ?> تاقیکردنەوەی پۆلی ١٢ - هەرێمی کوردستان</p>
+        </div>
+    </footer>
+    <?php wp_footer(); ?>
+</body>
+</html>
