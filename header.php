b2759681095df614e3b1b8c6796cb11ecac1417c
Mohamadfars <147287843+Mohamadfars@users.noreply.github.com>
2025-04-20 21:08:13 +0300

    header.php

---
+++ header.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?> dir="rtl">
+<head>
+    <meta charset="<?php bloginfo('charset'); ?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <?php wp_head(); ?>
+</head>
+<body <?php body_class(); ?>>
+    <header class="header">
+        <div class="container">
+            <h1><?php bloginfo('name'); ?></h1>
+            <p><?php bloginfo('description'); ?></p>
+        </div>
+    </header>
